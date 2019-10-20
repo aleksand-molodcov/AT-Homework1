b412c6d53ce680d6063abdf1f8eaa4150031f6a5
aleksand-molodcov <aleksand-molodcov@yandex.ru>
2019-10-20 17:43:05 +0400

    1

---
+++ test-stepen.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const stepen = require('./src/index.js');
+
+it('should produce a number to a power', () => {
+  assert.strictEqual(stepen('2', '3'), '80');
+});
+
+it('should produce a number to a power', () => {
+  assert.strictEqual(stepen('5', '4'), '625');
+});
+
+it('should produce a number to a power', () => {
+  assert.strictEqual(stepen('4', '2'), '8');
+});
+
+it('should produce a number to a power', () => {
+  assert.strictEqual(stepen('3', '3'), '27');
+});
